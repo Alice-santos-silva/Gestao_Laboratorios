57b732f1f228aa63a819d823b92d58ae2063c305
Alice Santos <alicesantossilva345@gmail.com>
2024-03-25 22:40:12 -0300

    commit inicial

---
+++ Views/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GestaoDeLaboratorios.DAL;
+using GestaoDeLaboratorios.Models;
+
+namespace GestaoDeLaboratorios.Views
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly GestaoDeLaboratorios.DAL.InfnetDbContext _context;
+
+        public DetailsModel(GestaoDeLaboratorios.DAL.InfnetDbContext context)
+        {
+            _context = context;
+        }
+
+        public Computadores Computadores { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var computadores = await _context.Computadores.FirstOrDefaultAsync(m => m.Id == id);
+            if (computadores == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Computadores = computadores;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Views/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GestaoDeLaboratorios.DAL;
+using GestaoDeLaboratorios.Models;
+
+namespace GestaoDeLaboratorios.Views
+{
+    public class IndexModel : PageModel
+    {
+        private readonly GestaoDeLaboratorios.DAL.InfnetDbContext _context;
+
+        public IndexModel(GestaoDeLaboratorios.DAL.InfnetDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Computadores> Computadores { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Computadores = await _context.Computadores.ToListAsync();
+        }
+    }
+}
